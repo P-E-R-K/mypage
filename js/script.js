38d9f0e22fea63fa6b29a807f7354e7c9af0ba06
Pieter Edward <poeri1231@gmail.com>
2023-04-02 16:00:22 +0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//Toggle class active
+
+const navbarNav = document.querySelector(".navbar-nav");
+
+//When Hamburger Menu clicked
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Close Menu when click outside
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
